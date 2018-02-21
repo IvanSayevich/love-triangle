27a59f498217025fc1fdb3bb54e126be7174419f
IvanSayevich <sundial@tut.by>
2018-02-21 12:38:29 +0300

    love Triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  //for our convenience---------------------------array indexing  will look like:  0,1,2  =>  0,1,2,3
+  //add a king to the Spichland country, so our preferences array will look like: [2,3,1] => [0,2,3,1]
+  preferences.unshift(0);
+  //add a sum variable to count our unhappy lovers
+  let unhappyLovers = 0;
+  //add a function that will find unhappy 'love triangles phenomenons' Spichonee
+  function findSpichonee(belovedPerson, spichonee, spichland) {
+    let spichoneeA = spichland[spichonee];
+        spichoneeB = spichland[spichoneeA];
+        spichoneeC = spichland[spichoneeB];
+    if(spichonee === spichoneeC && spichoneeA !== spichoneeB){
+        unhappyLovers += 1;
+    }
+  }
+  preferences.forEach(findSpichonee);
+  //we found out unhappy Spichonees and now we can return number of triangles  
+  return unhappyLovers/3;
 };
